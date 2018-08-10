df0d14162292468e2936d7a427486ed0426d86f1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-10 03:43:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,9 +5,10 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@all < self
   end
 
   def self.find_or_create_by_name(name)
-    
+    @@all.each do |artist|
   end
 end
